1c9e96f1aa00b384ecb3d595a70a3f654a2912f1
Maha1201 <abdalla.m1201@gmail.com>
2023-05-02 16:41:09 +0200

    💚 Create Ingredient 💚

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -3,17 +3,41 @@
 namespace App\Controller;
 
 use App\Repository\IngredientRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class IngredientController extends AbstractController
 {
-    #[Route('/ingredient', name: 'app_ingredient')]
-    public function index(IngredientRepository $repository): Response
+    /**
+     * This function displays all ingredients
+     * 
+     * @param IngredientRepository $repository
+     * @param PaginatorInterface $paginator
+     * @param Request $request
+     * @param Response
+     */
+
+    #[Route('/ingredient', name: 'app_ingredient', methods: ['GET'])]
+    public function index(IngredientRepository $repository, PaginatorInterface $paginator, Request $request): Response
     {
+        // knpPaginator pour les pages, 10 ingrédients par page
+        $ingredients = $paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt('page', 1),
+            10
+        );
+
         return $this->render('pages/ingredient/index.html.twig', [
-            'ingredients' => $repository->findAll()
+            'ingredients' => $ingredients
         ]);
+    }
+
+    #[Route('/ingredient/nouveau', 'ingredient.new', methods: ['GET', 'POST'])]
+    public function new(): Response
+    {
+        return $this->render('pages/ingredient/new.html.twig');
     }
 }
