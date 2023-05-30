b83e1cd03675556511389fc65b90a1639fcf463f
Maha1201 <abdalla.m1201@gmail.com>
2023-05-30 10:14:19 +0200

    2.58

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,17 +2,37 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\RecipeRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class RecipeController extends AbstractController
 {
+
+    /**
+     * This controller displays all recipes
+     * 
+     * @param RecipeRepository $repository
+     * @param PaginatorInterface $paginator
+     * @param Request $request
+     * @return Response
+     */
+    
     #[Route('/recette', name: 'recipe.index', methods: ['GET'])]
-    public function index(): Response
+    public function index(RecipeRepository $repository, PaginatorInterface $paginator, Request $request): Response
     {
+        // knpPaginator pour les pages, 10 ingrédients par page
+        $recipes = $paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt('page', 1),
+            10
+        );
+
         return $this->render('pages/recipe/index.html.twig', [
-            'controller_name' => 'RecipeController',
+            'recipes' => $recipes,
         ]);
     }
 }
